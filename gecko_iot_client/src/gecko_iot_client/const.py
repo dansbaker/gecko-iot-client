0b543626960c2eeeca829ddde7bb61752f5aa0d0
alex-reid-gecko <areid@geckoal.com>
2025-12-08 14:08:59 -0500

    chore: Use prod URLs

--- gecko_iot_client/src/gecko_iot_client/const.py
+++ gecko_iot_client/src/gecko_iot_client/const.py
@@ -1,3 +1,3 @@
 # API endpoints
-API_BASE_URL = "https://dev.api.geckowatermonitor.com"
-AUTH0_BASE_URL = "https://gecko-stage.us.auth0.com"
+API_BASE_URL = "https://api.geckowatermonitor.com"
+AUTH0_BASE_URL = "https://gecko-prod.us.auth0.com"
